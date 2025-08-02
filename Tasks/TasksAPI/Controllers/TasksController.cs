f5804be7fc150283475e8979417136cc918c1528
roberto freire <robertofreireferrazpassos@gmail.com>
2025-08-02 12:52:33 -0300

    remove temporary try catch code

--- Tasks/TasksAPI/Controllers/TasksController.cs
+++ Tasks/TasksAPI/Controllers/TasksController.cs
@@ -11,30 +11,6 @@ public class TasksController(ITasksRepository tasksRepository) : Controller
     [HttpGet("tasks/{type}")]
     public async Task<IActionResult> GetTasks(string type)
     {
-        var result = string.Empty;
-        try
-        {
-            //  Block of code to try
-            throw new ArgumentNullException("test");
-            result += "Okay";
-        }
-        catch (ArgumentNullException e)
-        {
-            //  Block of code to handle errors
-            result += "ArgumentNullException";
-            return Ok(result);
-        }
-        catch (Exception e)
-        {
-            //  Block of code to handle errors
-            result += "Catch";
-        }
-        finally
-        {
-            result += "Finally";
-        }
-
-        return Ok(result);
         return Ok(tasksRepository.GetTasks((TaskTypeEnum)System.Enum.Parse(typeof(TaskTypeEnum), type, ignoreCase: true)));
     }
 }
